b79a33df6d2827bd1ac19736016f9d1268039705
miapisani <146850097+miapisani@users.noreply.github.com>
2024-02-28 11:53:31 -0500

    functions

--- script.js
+++ script.js
@@ -1,61 +1,3 @@
-var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/main/Geography/Countries%20and%20Territories.csv"
-var country = getColumn(url,1);
-var countryCode = getColumn(url,3);
-var regions = getColumn(url,4);
-var income = getColumn(url,5);
-var population = getColumn(url,6);
-var gdp = getColumn(url,9);
-
-function getCountry(region){
-  var matches = [];
-  for(var i=0; i<regions.length; i++){
-    if(regions[i].toLowerCase().includes(region.toLowerCase())){
-      matches.push(country[i]);
-    }
-  }
-  return matches;
-}
-//console.log(getCountry("South Asia"))
-
-function highestGDP(region){
-  var highest = 0;
-  var richestCountry = "";
-for(var i = 0; i < regions.length; i++){
-  if(regions[i] == region){
-
-  }
-  if((regions[i] == region && parseFloat(gdp[i]) >= highest)){
- 
-    richestCountry = country[i];
-    highest = parseFloat(gdp[i]);
-  
-  }
-
-}
-if(richestCountry != ""){
-  return richestCountry;
-}
-return "";
-}
-
-console.log(highestGDP("North America"))
-
-// function getOldestTeam(division){
-//   var oldest = 3000;
-//   var oldestTeamname
-// for(var i = 0; i < divisions.length; i++){
-//   if((divisions[i] == division && joined[i] <= oldest)){
-//     oldestTeamname = team[i];
-//     oldest = joined[i];
-//   }
-
-// }
-// if(total == 0){
-//   return -1;
-// }
-// return total;
-// }
-
 
 //this is the utils file 
 function getColumn(url, columnNumber){
@@ -85,3 +27,55 @@ function getColumn(url, columnNumber){
 
 
       //this is the link to our country document 
+      var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/main/Geography/Countries%20and%20Territories.csv"
+
+//these are the variables we will use from our document 
+  var country = getColumn(url,1)
+  var countryCode = getColumn(url,3)
+  var region = getColumn(url,4)
+  var incomeLevel = getColumn(url,5)
+  var populations = getColumn(url,6)
+  var gdp = getColumn(url,9)
+
+//this function takes an inputted income level as its parameter and returns the countries within the level
+  function getCountryFromIncome (income){
+    var matches = [];
+    for(var i = 0; i < incomeLevel.length; i++){
+      if (incomeLevel[i].toLowerCase().includes(income.toLowerCase())){
+        matches.push(country[i]);
+      }
+    }
+    return matches;
+  }
+  // console.log (getCountryFromIncome("high")) 
+  // console.log (getCountryFromIncome("middle"))   
+
+
+
+  //this function takes an inputted country and returns its 2 letter country code 
+  function countryCodeFromCountry(countryName){
+    var matches = [];
+    for(var i = 0; i < country.length; i++){
+      if (country[i].toLowerCase().includes(countryName.toLowerCase())){
+        matches.push(countryCode[i]);
+  }
+}
+return matches;
+}
+// console.log (countryCodeFromCountry("brazil")); 
+
+//this function takes an inputted country name and prints its population 
+function countryPopulation (countryName){
+  var population = 0; 
+  for(var i = 0; i < country.length; i++){
+    if(country[i].toLowerCase().includes(countryName.toLowerCase())){
+      population=(populations[i])
+    }
+  }
+  return population
+
+}
+console.log(countryPopulation("United States"));  
+
+
+
